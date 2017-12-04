3e2791d08392dee98d65fc3da46dee404f1688e0
FORMACION PARA EL EMPLEO <FPE@aeg.edu>
2017-12-04 17:38:28 +0100

    Añadida rutas de prueba

--- Aplicacion/routes/Prueba.js
+++ Aplicacion/routes/Prueba.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET prueba page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Tomatico' });
+  res.render('prueba', { Estado: 'Activa' });
 });
 
 module.exports = router;
