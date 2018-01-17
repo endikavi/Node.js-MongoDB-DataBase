33ed2d9680e612334ca2dbe573641fe737c8b2d5
endikavi <endika.vi@gmail.com>
2018-01-17 05:15:42 +0100

    Vaya puto lio

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// Node Modules //
+
 const express = require('express');
 const path = require('path');
 const favicon = require('serve-favicon');
@@ -5,28 +7,31 @@ const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose')
+
+// Modules //
+
+const User = require ('./modules/user-schema');
+
+// Constants //
 const jsonParser = bodyParser.raw();
 const urlencodedParser = bodyParser.urlencoded({extended: false})
-let index = require('./routes/index');
-let users = require('./routes/users');
-let prueba = require('./routes/Prueba');
+const index = require('./routes/index');
+const users = require('./routes/users');
+const prueba = require('./routes/Prueba');
 const mongodbRoute ='mongodb://endika:endika@ds149865.mlab.com:49865/base_datos_aeg';
 const port = 8080;
 const app = express();
-
-// Modulos
-const User = require ('./moduloesquema');
-
-/*MONGODB*/
-const options = {
+const mongodbOptions = {
 	useMongoClient: true,
 	socketTimeoutMS: 0,
 	keepAlive: true,
 	reconnectTries: 30
 };
 
+// MongoDB Connection //
+
 mongoose.Promise = global.Promise
-mongoose.connect(mongodbRoute, options, (err) => {
+const db = mongoose.connect(mongodbRoute, mongodbOptions, (err) => {
 	if (err) {
 		return console.log(`Error al conectar a la base de datos: ${err}`)
 	}
@@ -36,109 +41,99 @@ mongoose.connect(mongodbRoute, options, (err) => {
 	console.log(`Conexión correcta.`)
 });
 
-// view engine setup
+// view engine setup //
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-	extended: false
-}));
+app.use(urlencodedParser);
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// route for pong //
+
 app.get('/pong', function (req ,res){
     console.log('Pedido el pong')
-    res.render('pong', { Estado: 'Activa' })})
+    res.render('pong')})
 
+// route to obtain all users //
 
-//ruta para ver usuarios
 app.get('/user', function(req, res) {
-    
     User.find().lean().exec(function (err, Users) {
         if (err) return console.error(err);
-        let Data = JSON.stringify(Users);
         console.log('Se pidio la lista de usuarios,actualmente contiene ' + Users.length + ' usuarios');
-        res.send('{"users":' + Data + '}');
-        
+        res.send('{"users":' + JSON.stringify(Users) + '}');        
 })
 })
 
-//ruta para busquedas
-app.post('/userfind', urlencodedParser, function(req, res) {
-    
+// route for searchs //
+
+app.post('/userfind', urlencodedParser, function(req, res) {    
     User.find(req.body).lean().exec(function (err, Users) {
         if (err) return console.error(err);
-        let Data = JSON.stringify(Users);
         console.log(req.body)
         console.log('Se busco en la lista de usuarios,resultados: ' + Users.length);
-        res.send('{"users":' + Data + '}');
-        
+        res.send('{"users":' + JSON.stringify(Users) + '}');        
 })
 })
 
-//ruta para añadir una array con los usuarios
-app.post('/userarray', urlencodedParser ,function(req, res) {
-    
-    req.body.map(element =>{
-    
-    const NewUser = new User();
- 
-    Object.assign (NewUser,element);
-    
-    NewUser.save(function (err, NewUser) {
-        if (err) return console.error(err);
-    })})
+// route to add users //
 
-	res.send('Añadido array de ' + req.body.length + ' usuarios');
-    console.log('Añadido array de ' + req.body.length + ' usuarios');
-    
-})
-
-//ruta para añadir usuario
 app.post('/user', urlencodedParser, function(req, res) {
+    if(req.body.isArray){
+		req.body.map(element =>{
+    	const NewUser = new User();
+    	Object.assign (NewUser,element);
+    	NewUser.save()
+			.then(user => {})
+			.catch(error => {} )
+		})
+		res.send('Añadidos ' + req.body.length + ' usuarios');
+        console.log('Añadidos ' + req.body.length + ' usuarios');	
+	}else{
+    	const NewUser = new User();
+    	Object.assign (NewUser,req.body);
+    	NewUser.save()
+    		.then(user => {
+				res.send(user);
+        		console.log('Añadido usuario:');
+				console.log(user);
+			})
+    		.catch(error => {} )
     
-    const NewUser = new User();
- 
-    Object.assign (NewUser,req.body);
-    
-    NewUser.save()
-	
-    .then(user => {
-       console.log('Usuario añadido:')
-       console.log(user)
-	   res.send(user);
-    })
-    
-    .catch(error => {} )
-    
-})
+}})
 
-//ruta alternativa recibir _id en la url
-app.delete('/user/:_id', function (req, res){
-    
+// route to delete users by id in the url //
+
+app.delete('/user/:_id', function (req, res){ 
     User.remove({ _id: req.params._id }, function (err) {
         if (err) return handleError(err);
     });
-    
 	console.log('Usuario eliminado ID: ' + req.params._id);
-	res.send('Usuario eliminado ID: ' + req.params._id);    
-    
+	res.send('Usuario eliminado ID: ' + req.params._id);      
 })
 
 //ruta para eliminar varios usuarios
+
 app.delete('/userdel', function (req, res){
-    
-    let Filtro = JSON.stringify(req.body);
-    console.log(Filtro);
-    console.log(res);
-    User.remove(Filtro , function (err) {
-        if (err) return handleError(err);
-    });
-    
-	res.send(res);    
+    array = [];
+    let obj = JSON.stringify(req.body);
+	JSON.parse(obj, function (x,y){
+	let z = " : "	
+	let newobj = (x + z + y);
+	array.push (JSON.parse(JSON.stringify((newobj))));	
+	})
+	array.pop();
+    console.log(array);
+  
+	array.map(element =>{
+    	User.remove(element , function (err) {
+			if (err) return handleError(err);
+		});
+	});
+	res.send(array); 
     
 })
 
@@ -176,26 +171,25 @@ app.path('/useremail/_id', function(req, res) {
 	res.send('email actualizado: ' + req.body.email);
 })
 
-// catch 404 and forward to error handler
+// catch 404 and forward to error handler //
+
 app.use(function(req, res, next) {
 	let err = new Error('Not Found');
 	err.status = 404;
 	next(err);
 });
 
-//cross-domain error fix
-app.use(bodyParser.urlencoded({
-	extended: false
-}))
-app.use(bodyParser.json())
+// cross-domain error fix //
+
 app.use(function(req, res, next) {
 	res.header('Access-Control-Allow-Origin', "*");
-	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH');
 	res.header('Access-Control-Allow-Headers', 'Content-Type');
 	next();
 })
 
-// error handler
+// error handler //
+
 app.use(function(err, req, res, next) {
 	// set locals, only providing error in development
 	res.locals.message = err.message;
@@ -204,5 +198,7 @@ app.use(function(err, req, res, next) {
 	res.status(err.status || 500);
 	res.render('error');
 });
+
+// exports //
 
 module.exports = app;
