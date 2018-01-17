02e620d5fd71ba6d04c8880a3e7e48d81bb17670
FORMACION PARA EL EMPLEO <FPE@aeg.edu>
2018-01-17 17:55:11 +0100

    hotfix 1

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -13,7 +13,7 @@ function llamadaInicial() {
     btnModos();
     printBoxTotal();
     table = false;
-    llamada(true, "GET", "https://prueba-base-de-datos-endika.herokuapp.com/user", "TEXT");
+    llamada(true, "GET", "https://prueba-base-de-datos-endika.herokuapp.com/user", "JSON");
   }else{alert('Sesion no iniciada')}
 };
 //llamada ajax al servidor       ***************************************************************************
@@ -28,7 +28,7 @@ function llamada(data, type, url, dataType) {
             console.log("La solicitud se ha completado correctamente.");
         
             if (type == "GET") {
-                obj = JSON.parse(JSON.stringify(user));
+                obj = JSON.parse(JSON.stringify(data));
           
             } else if (type == "DELETE") {
                 console.log("Administrador borrado")
