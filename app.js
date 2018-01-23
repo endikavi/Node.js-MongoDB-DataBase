51f776e2214efea9c520e058c9b641bd5df6d3ee
Endika <endika.vi@gmail.com>
2018-01-23 16:56:37 +0100

    cambio en el middle

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // middleware //
 
-app.use('/user/:id', function (req, res, next) {
+app.use('/user', function (req, res, next) {
   console.log('Request Type:', req.method);
   next();
 });
