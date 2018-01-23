3be442f1568b741af7d0ec613eed9159ef2ac643
Endika <endika.vi@gmail.com>
2018-01-23 17:53:17 +0100

    dfgdfgdfg

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-/*const validator = require('validator');*/
+const validator = require('validator');
 const router = express.Router();
 
 // Modules //
