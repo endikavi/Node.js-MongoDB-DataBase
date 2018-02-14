b8ecc48051acef40ead1b60c704162162ca1b560
Endika <endika.vi@gmail.com>
2018-02-14 17:56:40 +0100

    completado la validacion de datos,fallo de alerta siempre activo introducir en el hbs,empezar con la creacion de datos adicionales,hasear la contraseña.

--- controllers/accountctrl.js
+++ controllers/accountctrl.js
@@ -1,9 +1,23 @@
 const Account = require('../models/user-account-schema');
 const accountValidator = require('../middlewares/validateaccount');
 const User = require('../modules/user-schema');
-
+const checker = require('../modules/checker')
 const problem_email_taked = "El email proporcionado ya esta vinculado a una cuenta.";
 const problem_username_taked = "El nombre de usuario indicado ya esta en uso.";
+
+// funcion checkeo //
+
+function repeatedEmail(data){
+    
+return data.email == this;
+    
+}
+
+function repeatedUsername(data){
+    
+return data.email == this;
+    
+}
 
 // Mostrar login //
 exports.renderLogin = (req , res) => {
@@ -32,17 +46,32 @@ exports.accountRegister = (req , res) => {
 	accountValidator.seeIfRepeated(req.body)
 		.then((data) =>{
     		if(check == true){
-				
-				
-				
-				
 				allproblems = ["Datos no validos:<br>"];
-				console.log(data);
-				res.send('Registrado');
+				email = data.find(repeatedEmail,req.body.email)
+				username = data.find(repeatedUsername,req.body.username)
+                console.log(username + email)
+				if( username !== undefined){checker.addproblem(1,problem_username_taked)}
+                if( email !== undefined){checker.addproblem(1,problem_email_taked)}
+                if( username !== undefined || email !== undefined){
+       			      console.log(checker.addproblem(2,""));
+       			      res.render('register', {alert: checker.addproblem(2,"")});
+    		    }else{
+                      createAccount(req.body);
+                      res.render('login', {alert: "Registro completo"});   
+                }
 				
 			}else{
        			console.log(check);
        			res.render('register', {alert: check});
     		}
 		})
+}
+
+// Crear datos complementarios y hasear la contraseña //
+
+function createAccount (user){
+    
+    
+    return
+    
 }
